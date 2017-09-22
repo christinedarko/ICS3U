6af532babfd26ad48ce8f6e7ea766b2e78e76af1
324714468 <324714468@C-3ZZHYR1.tdsb.on.ca>
2017-09-22 14:34:35 -0400

    Pushing hello world for the first time.

---
+++ Unit 1/src/darko/HelloWorld.java
@@ -0,0 +1,16 @@
+package darko;
+/**
+ * HelloWorld
+ *This is a program that outputs "Hello World" to the screen
+ *2017/09/21
+ * @author Christine Darko
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	System.out.print("Hello World!");
+    
+	}
+
+}
