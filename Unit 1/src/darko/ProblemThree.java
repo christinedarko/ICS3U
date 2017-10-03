ac986e0af1dfbb1b912389316114ca3350deb331
Christine <Christine@192.168.1.128>
2017-10-02 21:12:10 -0400

    i tried to do problem 2 but failed

--- Unit 1/src/darko/ProblemThree.java
+++ Unit 1/src/darko/ProblemThree.java
@@ -1,7 +1,12 @@
 package darko;
 
 import java.util.Scanner;
-
 public class ProblemThree {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+	}
 }
--- Unit 1/src/darko/ProblemTwo.java
+++ Unit 1/src/darko/ProblemTwo.java
@@ -7,13 +7,37 @@ public class ProblemTwo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Input a number for the height of the bow tie greater than or equal to 5");
+		System.out.print("Input a number for the height of the bow tie which is odd positive number that is greater than or equal to 5");
 		int height = sc.nextInt();
 		int width = height * 2;
 		
-		for (int i = 1; i <= width; )
+		for (int i=1; i <= ((height / 2) + 0); )
 		{
+			System.out.println("*");
+			for (int i1 = 1; i1 <= i1 ++;)
+			{
+				
+			}
+			i++;
 			
+			if (i == 2)
+			{
+				System.out.print("*");
+			}
+			else if (i == 3)
+			{
+				System.out.print("**");
+			}
+			else if (i == 4)
+			{
+				System.out.print("***");
+			}
+		}
+		
+		//For the width
+		for (int i = 1; i <= (width); i++ )
+		{
+			System.out.print("*");
 		}
 	}
 
