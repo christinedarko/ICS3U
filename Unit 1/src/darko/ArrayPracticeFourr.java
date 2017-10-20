32795effd0c9f2b93859a0219be19a50d6489a41
324714468 <324714468@C-3ZZHYR1.tdsb.on.ca>
2017-10-20 15:02:08 -0400

    Programs from unit One

--- Unit 1/src/darko/ArrayPracticeFourr.java
+++ Unit 1/src/darko/ArrayPracticeFourr.java
@@ -13,11 +13,11 @@ public class ArrayPracticeFourr {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Hi let's play a game about gymnastics skills. Think of a floor skill and press the enter key when you are ready.");
-		String answer = sc.nextLine();
+		sc.nextLine();
 		String [] skills = {"cartwheel", "handstand", "back handspring (back flip)", "split", "bridge", "candlestick", "aerial", "half turn", "front walkover", "back walkover", "front handspring (front flip)", "front handspring stepout", "press handstand", "back handspring stepout", "front aerial", "dive rolls", "back tuck", "front limber", "somersault (roll)", "round off", "straddle jump" };
 		int choice;
 		String response;
-		for (int i= 0; i > 20; i++)
+		for (int i = 0; i < 20; i++)
 		{
 			choice = (int)(Math.random()*20);
 			System.out.println("Is it a " + skills [choice]);
---
+++ Unit 1/src/darko/DealOrNoDeal.java
@@ -0,0 +1,29 @@
+package darko;
+
+import java.util.Scanner;
+/**
+ * @author 324714468
+ *
+ */
+public class DealOrNoDeal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("How many cases have been opened so far?");
+		int n = sc.nextInt();
+		System.out.println("Indicate which amounts were eliminated by writing 1 for $100, 2 for $500, 3 for $1000 and so on until 10 for $1000000. Between each amount press the enter key");
+		for (int i = 0; i < n; i++)
+		{
+			
+		}
+		int [] amnts = {100, 500, 1000, 5000, 10000, 25000, 50000, 100000, 500000, 1000000};
+		
+		
+		
+	}
+
+}
