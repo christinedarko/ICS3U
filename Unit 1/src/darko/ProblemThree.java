e783acbd48d8354c98f4113d65faa6d4594bdd60
Christine <Christine@192.168.1.131>
2017-11-04 21:32:29 -0400

    FINISHED ALL UNIT ONE PROGRAMSSSSSSSS

--- Unit 1/src/darko/ProblemThree.java
+++ Unit 1/src/darko/ProblemThree.java
@@ -1,12 +1,61 @@
 package darko;
 
 import java.util.Scanner;
+
+/**
+ * Program Name: Problem Three
+ * Date: November 4, 2017
+ * Description: 
+ * @author Christine Darko
+ *
+ */
+
 public class ProblemThree {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		Scanner sc = new Scanner(System.in);
+		System.out.println("How tall would you like the tines to be?");
+		int tines = sc.nextInt();
+		System.out.println("What would you like the spacing of the tines to be?");
+		int spacing = sc.nextInt();
+		System.out.println("How long would you like the handle to be?");
+		int handle = sc.nextInt();
 		
+		//Prints tines
+		for (int i = 0; i < tines; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				System.out.print("*");
+				for (int k = 0; k < spacing; k++)
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println("");
+
+		}
+		//prints centre line
+		for (int i = 0; i < spacing * 2 + 3; i++)
+		{
+			System.out.print("*");
+		}
+		System.out.println("");
+		
+		//prints handle
+		for (int i = 0; i < handle; i++)
+		{
+			for (int j = 0; j < spacing + 1; j++)
+			{
+				System.out.print(" ");
+			}
+			for (int k = 0; k < 1; k++)
+			{
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
 	}
 }
